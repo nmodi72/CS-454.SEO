48fd15cf5d1958dfc51a290bace43cd96cb8dc1e
Harshal Shah <harshalshah162@gmail.com>
2015-02-27 11:49:55 -0800

    Delete unwanted

--- sqlFile.sql
+++ sqlFile.sql
@@ -1,7 +1,12 @@
-use seo;
+use crawler;
 
 CREATE TABLE IF NOT EXISTS `Record` (
   `RecordID` INT(11) NOT NULL AUTO_INCREMENT,
   `URL` text NOT NULL,
   PRIMARY KEY (`RecordID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+use crawler;
+select * from record;
+
+delete from record;
